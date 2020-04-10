c2a8f50ddf5c66353c9ea3dcd1b4829f0ca5b79d
w1jtoo <wanadooht@gmail.com>
2020-04-10 14:34:50 +0500

    wip

--- src/main.nim
+++ src/main.nim
@@ -1,24 +1,26 @@
-import server, net, asyncdispatch
+# import server, net, asyncdispatch
 
 
-# import asynchttpserver, asyncdispatch, httpclient, uri
+import asynchttpserver, asyncdispatch, httpclient, uri, httpcore
 
 
 when isMainModule: 
-    let opts = ProxyServerOptions(listenAddr:"127.0.0.1", listenPort:8088.Port)
-    var f = newProxyServer(opts)
-    asyncCheck f.serve()
-    runForever()
-    
-    
-    # proc cb(req: Request) {.async.} =
-    #     echo req
-    #     if req.reqMethod == HttpGet:
-    #         var client = newHttpClient()
-    #         let content =  client.getContent($req.url)
-    #         await req.respond(Http200, content)
-
-    # var server = newAsyncHttpServer() 
-    
-    # asyncCheck server.serve(Port(8088), cb)
+    # let opts = ProxyServerOptions(listenAddr:"127.0.0.1", listenPort:8088.Port)
+    # var f = newProxyServer(opts)
+    # asyncCheck f.serve()
     # runForever()
+    
+    
+    proc cb(req: Request) {.async.} =
+        var client = newHttpClient()
+        echo req.headers["host"]
+        echo $req.reqMethod
+        let response = client.request("http://" & req.headers["host"], $req.reqMethod)
+        echo response.body
+        echo response.headers
+        await req.respond(Http200, $response.headers)
+
+    var server = newAsyncHttpServer() 
+    
+    asyncCheck server.serve(Port(8088), cb)
+    runForever()
